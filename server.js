d4516fc5c893849ec836b38f0bc83fccc986aa11
abhijit2712 <74667882+abhijit2712@users.noreply.github.com>
2024-04-29 12:06:09 +0530

    server

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/api/v1/product',productRoutes);
 // app.use('*',function(req,res){
 //     res.sendFile(path.join(__dirname,"./client/build/index.html"))
 // })
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
