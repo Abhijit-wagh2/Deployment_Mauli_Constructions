765b1614c6804cd60c7c9505e996d31ee7658960
abhijit2712 <74667882+abhijit2712@users.noreply.github.com>
2024-05-03 13:48:02 +0530

    cors backend

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import connectDB from "./config/db.js";
 import router from "./routes/authRoute.js";
 import categoryRoute from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoutes.js";
-
+import cors from "cors";
 import { fileURLToPath } from "url";
 
 import path from 'path';
@@ -21,8 +21,10 @@ const app = express();
 
 
 //middleware
+app.use(cors());
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname,"./client/build")))
 
 //routes
 app.use('/api/v1/auth',router);
